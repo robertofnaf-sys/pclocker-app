26a387c3d29c479a86eace8adce0a0195cce127f
robert <robertofnaf@gmail.com>
2026-05-03 02:22:18 -0600

    Sincronizar nombres de variables de entorno con Vercel

--- server.js
+++ server.js
@@ -122,6 +122,7 @@ app.put('/api/prestamos/:id', (req, res) => {
 });
 
 // ==========================================
-app.listen(3000, () => {
-    console.log('🚀 Servidor API corriendo en el puerto 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
 });
